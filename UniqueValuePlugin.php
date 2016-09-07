331f9ed8a5f85b06c08d206071fdaf8c74181cb0
David <davidyew@users.noreply.github.com>
2016-09-08 03:40:00 +0800

    added defineAdditionalEntryTableAttributes() so that the columns will be shown for entry table columns list

--- UniqueValuePlugin.php
+++ UniqueValuePlugin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Craft;
 
 /**
@@ -9,28 +10,47 @@ namespace Craft;
  * @copyright Copyright (c) 2014, Josh Angell
  * @link      http://www.joshangell.co.uk
  */
-
 class UniqueValuePlugin extends BasePlugin
 {
 
-  public function getName()
-  {
-    return Craft::t('Unique Value');
-  }
+    public function getName()
+    {
+        return Craft::t('Unique Value');
+    }
 
-  public function getVersion()
-  {
-    return '1.2';
-  }
+    public function getVersion()
+    {
+        return '1.2';
+    }
 
-  public function getDeveloper()
-  {
-    return 'Josh Angell';
-  }
+    public function getDeveloper()
+    {
+        return 'Josh Angell';
+    }
 
-  public function getDeveloperUrl()
-  {
-    return 'http://www.joshangell.co.uk';
-  }
+    public function getDeveloperUrl()
+    {
+        return 'http://www.joshangell.co.uk';
+    }
+
+    /**
+     * Additional table columns available to entry indexes
+     * 
+     * @link https://craftcms.com/docs/plugins/hooks-reference#defineAdditionalEntryTableAttributes Hooks' reference on defineAdditionalEntryTableAttributes
+     * 
+     * @return array
+     */
+    public function defineAdditionalEntryTableAttributes()
+    {
+        $array = [];
+
+        foreach (craft()->fields->getFieldsWithContent() as $field) {
+            if ($field->type == $this->getClassHandle()) {
+                $array[$field->handle] = $field->name;
+            }
+        }
+
+        return $array;
+    }
 
 }
